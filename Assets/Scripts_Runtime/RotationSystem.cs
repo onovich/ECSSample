2f8684f89b834532f57a885e8f1ce973ee5923ff
onovich <hjn1110@163.com>
2024-05-03 17:32:47 +0800

    <feature> add: using Aspect

--- Assets/Scripts_Runtime/RotationSystem.cs
+++ Assets/Scripts_Runtime/RotationSystem.cs
@@ -1,5 +1,6 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace HelloCube {
@@ -13,13 +14,25 @@ namespace HelloCube {
         [BurstCompile]
         public void OnUpdate(ref SystemState state) {
             float deltaTime = SystemAPI.Time.DeltaTime;
-
+            double elapsedTime = SystemAPI.Time.ElapsedTime;
             foreach (var (transform, speed) in
                      SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotSpeedComponent>>()) {
-                transform.ValueRW = transform.ValueRO.RotateY(
-                    speed.ValueRO.RadiansPerSecond * deltaTime);
+                transform.ValueRW = transform.ValueRO.RotateY(speed.ValueRO.RadiansPerSecond * deltaTime);
+            }
+            foreach (var movement in
+                     SystemAPI.Query<VerticalMovementAspect>()) {
+                movement.Move(elapsedTime);
             }
         }
-
     }
+
+    readonly partial struct VerticalMovementAspect : IAspect {
+        readonly RefRW<LocalTransform> m_Transform;
+        readonly RefRO<RotSpeedComponent> m_Speed;
+
+        public void Move(double elapsedTime) {
+            m_Transform.ValueRW.Position.y = (float)math.sin(elapsedTime * m_Speed.ValueRO.RadiansPerSecond);
+        }
+    }
+
 }
